171a661f6cc18378f4bdddc39c853e34228baaed
Andres Gomez <andres2661991@gmail.com>
2023-04-22 14:43:22 -0300

    Detail Page

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
 import 'package:social_cook/src/providers/user.dart';
-
+import 'package:social_cook/src/screens/detail.dart';
 
 import 'package:social_cook/src/screens/homePage.dart';
 import 'package:social_cook/src/screens/login.dart';
@@ -18,29 +18,27 @@ class MyApp extends StatelessWidget {
   final ThemeData theme = ThemeData();
   @override
   Widget build(BuildContext context) {
-    
     return MultiProvider(
       providers: [
         ListenableProvider(create: (context) => User()),
       ],
       child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: theme.copyWith(
-          colorScheme: theme.colorScheme.copyWith(
-            primary: Colors.cyan,
-            secondary: Colors.white,
+          title: 'Flutter Demo',
+          theme: theme.copyWith(
+            colorScheme: theme.colorScheme.copyWith(
+              primary: Colors.cyan,
+              secondary: Colors.white,
+            ),
           ),
-        ),
-        initialRoute: "/",
-        routes: {
-          "/": (context) =>  const MyLoginPage(title: "Login",),
-           "/homePage": (context) =>  const HomePage(),
-           "/register": (context) =>  const Register(),
-    
-        }
-      ),
+          initialRoute: "/",
+          routes: {
+            /* "/": (context) => const MyLoginPage(
+                  title: "Login",
+                ),
+            "/homePage": (context) => const HomePage(),
+            "/register": (context) => const Register(), */
+            "/": (context) => const Detail(),
+          }),
     );
   }
 }
-
-
