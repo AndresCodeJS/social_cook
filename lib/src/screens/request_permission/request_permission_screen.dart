5f9177f764c09fbb2bedff6e1ba579d35348289e
Andres Gomez <andres2661991@gmail.com>
2023-04-27 23:16:41 -0300

    Permisos de ubicación

---
+++ lib/src/screens/request_permission/request_permission_screen.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import '../../ui/routes/routes.dart';
+import 'request_permission_controller.dart';
+
+class RequestPermisionScreen extends StatefulWidget {
+  const RequestPermisionScreen({super.key});
+
+  @override
+  State<RequestPermisionScreen> createState() => _RequestPermisionScreenState();
+}
+
+class _RequestPermisionScreenState extends State<RequestPermisionScreen> {
+  final _controller = RequestPermissionController(Permission.locationWhenInUse);
+  late StreamSubscription _suscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _suscription = _controller.onStatusChanged.listen((status) {
+      if (status == PermissionStatus.granted) {
+        Navigator.pushReplacementNamed(context, Routes.HOME);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _suscription.cancel();
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Container(
+        height: double.infinity,
+        width: double.infinity,
+        alignment: Alignment.center,
+        child: ElevatedButton(
+          child: const Text("Allow"),
+          onPressed: () {
+            _controller.request();
+          },
+        ),
+      ),
+    ));
+  }
+}
